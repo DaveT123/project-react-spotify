3dea0824f697401a6916658be08e6b0674f9ab27
DaveT123 <sinchin9001@hotmail.com>
2022-12-15 01:01:02 +0800

    fix logic for access token

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -3,7 +3,7 @@ const redirectUri = "https://davet123.github.io/project-react-spotify/";
 let accessToken;
 
 const Spotify = {
-    getAccessToken(logIn = false) {
+    getAccessToken() {
         if (accessToken) {
             return accessToken;
         }
@@ -21,13 +21,11 @@ const Spotify = {
             window.history.pushState("Access Token", null, "/");
         } else {
             const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
-            if (!logIn) {
-                window.location = accessUrl;
-            }
+            window.location = accessUrl;
         }
     },
     search(term) {
-        const accessToken = Spotify.getAccessToken(true);
+        const accessToken = Spotify.getAccessToken();
         return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
             headers: {
                 Authorization: `Bearer ${accessToken}`,
